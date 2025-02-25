a9f041228488f2fd035bcbef1ded7317ecad0e02
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-25 18:38:06 +0300

    Add placeholder for Coming-soon component

---
+++ src/components/Coming-soon.jsx
@@ -0,0 +1,9 @@
+// import { useState, useEffect } from "react";
+// import { Button } from "@/components/ui/button";
+
+// export default function BottomAlertButton() {
+
+//   return (
+  
+//   );
+// }
